08670e4535bd56b6e8d3e1288cb9ff534a6f3ecc
shekhar-patil <patilshekhar900@gmail.com>
2019-02-14 13:43:08 +0530

    Updates

---
+++ precache-manifest.453fc3312768961e38155195b0181e7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tictoctoe/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1814d9886023f948f638",
+    "url": "/tictoctoe/static/js/runtime~main.1814d988.js"
+  },
+  {
+    "revision": "52a66014636e651cfe30",
+    "url": "/tictoctoe/static/js/main.52a66014.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/tictoctoe/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "52a66014636e651cfe30",
+    "url": "/tictoctoe/static/css/main.4d109fac.chunk.css"
+  },
+  {
+    "revision": "922c778b0eccbb30cea629a28b567e0a",
+    "url": "/tictoctoe/index.html"
+  }
+];
